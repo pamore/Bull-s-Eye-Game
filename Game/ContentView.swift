5f3130438f221b0c7a5b7dae1dbe6ed0b54ae6b2
prachimore1 <prachi.more@integral.io>
2019-10-06 17:03:31 -0400

    Initial Commit

---
+++ Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game
+//
+//  Created by Prachi on 10/6/19.
+//  Copyright © 2019 Integral. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
